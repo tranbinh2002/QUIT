3e2d83e6de48467c16d4222a3c4d080fc36fd6af
BinhTran <fam.out16@gmail.com>
2025-08-23 14:46:41 +0700

    player movement

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController controller;
+
+    const float GRAVITY = 9.81f;
+    bool isJumping;
+    int currentJumpDirection = 1; //hướng nhảy hiện tại: nhảy lên
+    float velocity = 5f;
+    Vector3 currentMoveDirection;
+    float jumpHeight = 0.5f;
+
+    public void SetUp(float playerVelocity, float playerJumpHeight)
+    {
+        velocity = playerVelocity;
+        jumpHeight = playerJumpHeight;
+    }
+
+    private void Start()
+    {
+        while (!controller.isGrounded)
+        {
+            controller.Move(-GRAVITY * Time.deltaTime * Vector3.up);
+        }
+    }
+
+    void Update()
+    {
+        MoveHandle();
+        JumpHandle();
+    }
+
+    void MoveHandle()
+    {
+        Vector3 motion;
+        if (!isJumping)
+        {
+            motion = velocity * Time.deltaTime
+            * Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+            currentMoveDirection = motion;
+            motion.y -= GRAVITY * Time.deltaTime;
+        }
+        else
+        {
+            motion = currentMoveDirection + JumpMotion();
+        }
+        controller.Move(motion);
+    }
+
+    const float safeValue = 0.005f;
+    Vector3 JumpMotion()
+    {
+        float h = jumpHeight - transform.position.y;
+        Debug.Log(h);
+        if (h - safeValue <= 0)
+        {
+            currentJumpDirection = -1;
+            Debug.Log("Change Jump Dir");
+        }
+        return currentJumpDirection * Time.deltaTime * Mathf.Sqrt(2 * GRAVITY * Mathf.Abs(h)) * Vector3.up;
+    }
+
+    void JumpHandle()
+    {
+        if (isJumping && controller.isGrounded)
+        {
+            isJumping = false;
+            currentJumpDirection = 1;
+        }
+
+        if (!isJumping && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            Debug.Log("Space");
+        }
+    }
+}
